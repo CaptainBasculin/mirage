61ee0baf5bb9d3bd0f97d09a5cf5e61207d7c696
Seth Stephens <seth@sethstephens.me>
2019-11-26 20:49:52 -0500

    fix invites

--- src/routes/AccountRouter.ts
+++ src/routes/AccountRouter.ts
@@ -124,9 +124,9 @@ AccountRouter.route('/invites').get((req, res) => {
 })
 AccountRouter.route('/invites/create').post(async (req, res) => {
   if (
-    !req.user.admin ||
-    !req.user.moderator ||
-    !req.user.inviteCreator ||
+    !req.user.admin &&
+    !req.user.moderator &&
+    !req.user.inviteCreator &&
     (req.user.availableInvites || 0) < 1
   ) {
     return res.redirect('/account/invites')
