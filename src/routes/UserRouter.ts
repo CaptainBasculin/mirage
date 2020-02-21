847f51ea2ebc489b5b76e845c4ee015ddadf5ad2
relative <relative@riseup.net>
2020-02-21 13:33:57 -0500

    List donated domains on user page

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { User } from '../database/entities/User'
+import { Domain } from '../database/entities/Domain'
 
 const UserRouter = express.Router()
 
@@ -7,12 +8,24 @@ UserRouter.route('/:user').get(async (req, res) => {
   let user = await User.findOne({
     where: {
       username: req.params.user
-    }
+    },
+    relations: ['donatedDomains']
   })
   if (!user) {
     return res.render('pages/user/notfound')
   }
-
+  const sortFn = (a: Domain, b: Domain) => {
+    if (a.domain < b.domain) {
+      return -1
+    }
+    if (a.domain > b.domain) {
+      return 1
+    }
+    return 0
+  }
+  if (user.donatedDomains) {
+    user.donatedDomains = user.donatedDomains.sort(sortFn)
+  }
   return res.render('pages/user/user', {
     user
   })
